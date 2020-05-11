5c9148c1da4a160d8d1571ee174906d06a6c7076
wis-w <1044957202@qq.com>
2020-05-11 08:20:34 +0800

    线程池

---
+++ studyForJob/src/main/java/juc/MyThreadPoolDemo.java
@@ -0,0 +1,14 @@
+package juc;
+/**
+ * @author wyg_edu
+ * @date 2020年5月11日 上午8:16:35
+ * @version v1.0
+ * 线程池作用，线程复用，减少资源开销，利于管理
+ */
+public class MyThreadPoolDemo {
+	
+	public static void main(String[] args) {
+		System.out.println();
+	}
+
+}
